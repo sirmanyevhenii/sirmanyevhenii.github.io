53013b5e7ee0275997243aa757122af1e4db8666
Yevhenii Sirman <sirmanj92@gmail.com>
2018-02-01 19:31:43 +0200

    add second tab

--- tabs/src/js/common.js
+++ tabs/src/js/common.js
@@ -7,7 +7,7 @@ function readURL(input) {
     var reader = new FileReader();
 
     reader.onload = function (e) {
-      $('#blah')
+      $('.blah')
         .attr('src', e.target.result)
         .css({"width":"100%", "display":"block"});
     };
@@ -17,93 +17,105 @@ function readURL(input) {
 }
 
 
-// $(function() {
 
-//     $("#name").bind("keyup", function(event) {
-//         var regex = /^[a-zA-Z]+(([\'\,\.\- ][a-zA-Z ])?[a-zA-Z]*)*$/;
-
-//         $( "p" ).remove();
-//         if (regex.test($("#name").val())) {
-//             $('.modal-body').append("<p>Name - valid.<p>");
-//         } else {
-//             $('.modal-body').append("<p>Name - This field should contain only lettetrs!<p>");
-//         }
-//     });
+$(document).ready(function(){
+        
+        var validName = false,
+            validEmail = false,
+            validCity = false,
+            validState = false,
+            validTextarea = false;
 
 
-//     $("#email").bind("keyup", function(event) {
-//         var regex = /.+@outlook.com|.+@gmail.com/;
+        $("form.form-1").submit(function(event){
+            event.preventDefault();
 
-//         $( "p" ).remove();
-//         if (regex.test($("#email").val())) {
-//             $('.modal-body').html('Email - valid.');
-//         } else {
-//             $('.modal-body').append("<p>Email - We'll accept only emails from gmail.com and outlook.com!<p>");
-//         }
-//     });
+            var name = $("#name-1").val(),
+                nameValid = /^[a-zA-Z\s]+$/,
+                email = $("#email-1").val(),
+                emailValid = /.+@outlook.com|.+@gmail.com/,
+                city = $("#city-1").val(),
+                state = $("#state-1").val(),
+                textarea = $("#textarea-1").val().length;
 
 
-//     // empty field
-
-//     $("#city").bind("keyup", function(event) {
-//         var regex = /^(\w+\S+)$/;
-//         if (regex.test($("#city").val())) {
-//             $('.modal-body').html('City field are filled.');
-//         } else {
-//             $('.modal-body').append("<p>City field are empty!<p>");
-//         }
-//     });
+            if(name == "") {
+              $("p.p-name").remove()
+              $('<p>', { class: 'p-name', text: '"Name" field are empty!'}).appendTo('.modal-body');
+              validName = false;
+            } else {
+              $("p.p-name").remove()
+              validName = true;
+            }
 
 
 
+            if (nameValid.test($("#name-1").val())) {
+              $("p.p-name-valid").remove()
+              validName = false;
+            } else {
+              $("p.p-name-valid").remove()
+              $('<p>', { class: 'p-name-valid', text: '"Name" field should contain only letters and spaces.'}).appendTo('.modal-body');
+              validName = true;
+            }
 
 
+            if(email == "") {
+              $("p.p-email").remove()
+              $('<p>', { class: 'p-email', text: '"Email" field are empty!'}).appendTo('.modal-body');
+              validEmail = false; 
+            } else {
+              $("p.p-email").remove()
+              validEmail = true;  
+            }
 
 
-    // $("#name").bind("keyup", function(event) {
-    //     var regex = /^[A-Za-zА-Яа-яЁё]+(\s[A-Za-zА-Яа-яЁё]+)*$/;
-    //     if (regex.test($("#name").val())) {
-    //         $('.modal-body').html('All field are filled.');
-    //     } else {
-    //         $('.modal-body').append("<p>Name field are empty!<p>");
-    //     }
-    // });
-
-    // $("#email").bind("keyup", function(event) {
-    //     var regex = /^[A-Za-zА-Яа-яЁё]+(\s[A-Za-zА-Яа-яЁё]+)*$/;
-    //     if (regex.test($("#email").val())) {
-    //         $('.modal-body').html('All field are filled.');
-    //     } else {
-    //         $('.modal-body').append("<p>Email field are empty!<p>");
-    //     }
-    // });
-
-    // $("#city").bind("keyup", function(event) {
-    //     var regex = /^[A-Za-zА-Яа-яЁё]+(\s[A-Za-zА-Яа-яЁё]+)*$/;
-    //     if (regex.test($("#city").val())) {
-    //         $('.modal-body').html('All field are filled.');
-    //     } else {
-    //         $('.modal-body').append("<p>City field are empty!<p>");
-    //     }
-    // });
-
-    // $("#state").bind("keyup", function(event) {
-    //     var regex = /^[A-Za-zА-Яа-яЁё]+(\s[A-Za-zА-Яа-яЁё]+)*$/;
-    //     if (regex.test($("#state").val())) {
-    //         $('.modal-body').html('All field are filled.');
-    //     } else {
-    //         $('.modal-body').append("<p>State field are empty!<p>");
-    //     }
-    // });
-
-    // $("#textarea").bind("keyup", function(event) {
-    //     var regex = /^[A-Za-zА-Яа-яЁё]+(\s[A-Za-zА-Яа-яЁё]+)*$/;
-    //     if (regex.test($("#textarea").val())) {
-    //         $('.modal-body').html('All field are filled.');
-    //     } else {
-    //         $('.modal-body').append("<p>Issue description field are empty!<p>");
-    //     }
-    // });
+            if(emailValid.test($("#email-1").val())) {
+              $("p.p-email-valid").remove()
+              validEmail = false; 
+            } else {
+              $("p.p-email-valid").remove()
+              $('<p>', { class: 'p-email-valid', text: '"Email" field should contain only emails from gmail.com and outlook.com !'}).appendTo('.modal-body');
+              validEmail = true;  
+            }
 
 
-// });
+            if(city == "") {
+              $("p.p-city").remove()
+              $('<p>', { class: 'p-city', text: '"City" field are empty!'}).appendTo('.modal-body');
+              validCity = false;
+            } else {
+              $("p.p-city").remove()
+              validCity = true;
+            }
+
+
+            if(state == "") {
+              $("p.p-state").remove()
+              $('<p>', { class: 'p-state', text: '"State" field are empty!'}).appendTo('.modal-body');
+              validState = false; 
+            } else {
+              $("p.p-state").remove()
+              validState = true;  
+            }
+
+
+            if(textarea < 5 || textarea > 300) {
+              $("p.textarea").remove()
+              $('<p>', { class: 'textarea', text: '"Textarea" field should contain min 5 characters, max 300 characters!'}).appendTo('.modal-body');
+              validTextarea = false; 
+            } else {
+              $("p.textarea").remove()
+              validTextarea = true;  
+            }
+
+
+            if(validName == true && validEmail == true && validCity == true && validState == true && validTextarea == true) {
+              $("form").unbind('submit').submit();
+            } else {
+              $('<p>').remove()
+            }
+
+        });
+
+    });
